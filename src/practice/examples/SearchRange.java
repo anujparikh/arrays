6e314f5ad0dc9bf41df290e8f83a13daa4e48589
Anuj Parikh <parikh_anuj@cat.com>
2020-01-02 15:17:25 -0600

    update documentation

--- src/practice/examples/SearchRange.java
+++ src/practice/examples/SearchRange.java
@@ -1,5 +1,16 @@
 package practice.examples;
 
+/**
+ * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * If the target is not found in the array, return [-1, -1].
+ *
+ * Example
+ * Input: nums = [5,7,7,8,8,10], target = 8; Output: [3,4]
+ * Input: nums = [5,7,7,8,8,10], target = 6; Output: [-1,-1]
+ *
+ * NOTE: Create solution using Binary search for achieving O(log n) time complexity
+ */
 public class SearchRange {
     public static int[] searchRange(int[] nums, int target) {
         int i = 0;
